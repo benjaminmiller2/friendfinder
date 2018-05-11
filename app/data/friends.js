0891a688c5bb61dc5c4caace3a4c7829e8fc8e90
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-05-11 12:56:44 -0500

    third, i think

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friendList = [
     {
         "name": "Michael Scott",
-        "photo": "pending",
+        "photo": "https://en.wikipedia.org/wiki/Michael_Scott_(The_Office)#/media/File:MichaelScott.png",
         "scores": [
             "1",
             "2",
@@ -14,5 +14,245 @@ let friendList = [
             "1",
             "1"
         ]
-    }
+    },
+    {
+        "name": "Dwight K. Schrute",
+        "photo": "https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "1",
+            "2",
+            "1",
+            "3",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        "name": "Jim Halpert",
+        "photo": "https://en.wikipedia.org/wiki/Jim_Halpert#/media/File:Jim-halpert.jpg",
+        "scores": [
+            "5",
+            "2",
+            "4",
+            "1",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Pam Beesly",
+        "photo": "https://en.wikipedia.org/wiki/Pam_Beesly#/media/File:Pam_Beesley.jpg",
+        "scores": [
+            "1",
+            "4",
+            "4",
+            "2",
+            "3",
+            "1",
+            "2",
+            "1",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Ryan Howard",
+        "photo": "https://en.wikipedia.org/wiki/Ryan_Howard_(The_Office)#/media/File:Ryan_Howard_(The_Office).jpg",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Andy Bernard",
+        "photo": "https://en.wikipedia.org/wiki/Andy_Bernard#/media/File:Andy_Bernard_photoshot.jpg",
+        "scores": [
+            "5",
+            "3",
+            "3",
+            "3",
+            "5",
+            "4",
+            "4",
+            "1",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Stanley Hudson",
+        "photo": "https://en.wikipedia.org/wiki/Stanley_Hudson#/media/File:Stanley_Hudson.jpg",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "3",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Kevin Malone",
+        "photo": "https://en.wikipedia.org/wiki/Kevin_Malone#/media/File:Office-1200-baumgartner1.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Meredith Palmer",
+        "photo": "https://en.wikipedia.org/wiki/Meredith_Palmer#/media/File:Meredith_Palmer.jpg",
+        "scores": [
+            "4",
+            "1",
+            "3",
+            "5",
+            "2",
+            "1",
+            "4",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Angela Martin",
+        "photo": "https://en.wikipedia.org/wiki/Angela_Martin#/media/File:Angela_Martin.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "4",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Oscar Martinez",
+        "photo": "https://en.wikipedia.org/wiki/Oscar_Martinez_(The_Office)#/media/File:Oscar_Martinez_of_The_Office.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "1",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Phyllis Lapin",
+        "photo": "https://en.wikipedia.org/wiki/Phyllis_Vance#/media/File:Phyllis_Lapin-Vance.jpg",
+        "scores": [
+            "1",
+            "2",
+            "1",
+            "5",
+            "2",
+            "1",
+            "2",
+            "1",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Kelly Kapoor",
+        "photo": "https://en.wikipedia.org/wiki/Kelly_Kapoor#/media/File:Kelly_Kapoor.jpg",
+        "scores": [
+            "4",
+            "4",
+            "5",
+            "2",
+            "3",
+            "3",
+            "3",
+            "4",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Toby Flenderson",
+        "photo": "https://en.wikipedia.org/wiki/Toby_Flenderson#/media/File:Toby_Flenderson_promo_picture.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "2",
+            "5",
+            "3",
+            "1",
+            "2",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Creed Bratton",
+        "photo": "https://en.wikipedia.org/wiki/Creed_Bratton_(character)#/media/File:CreedBratton(TheOffice).jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Darryl Philbin",
+        "photo": "https://en.wikipedia.org/wiki/Darryl_Philbin#/media/File:DarrylPhilbin.jpg",
+        "scores": [
+            "1",
+            "2",
+            "1",
+            "3",
+            "1",
+            "1",
+            "1",
+            "4",
+            "3",
+            "2"
+        ]
+    },
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,5 +9,21 @@ module.exports = function(app){
     app.get('/api/friends', function(req, res){
         res.json(friends);
     });
+
+app.post('/api/friends', function(req, res){
+
+    let userScores = req.body.scores;
+    let scoreArray = [];
+    let match = 0;
+
+    for(let i = 0; i < friendList.length; i++){
+        let difference = 0;
+
+        for(let j = 0; j < userScores.length; j++){
+            difference += (Math.abs(userScore[j] - friendList[i].scores[j]))
+        }
+    scoreArray.push(difference);
+    }
+})
 };
 
